8b204cdab84048f5c9e6c16f8438d567f97817ed
rmwcz7zzn6-eng <rmwcz7zzn6@privaterelay.appleid.com>
2026-08-23 23:48:26 +0200

    Fix emotion blends layout

--- 02_PRODUCTS/Emotion/app/service-worker.js
+++ 02_PRODUCTS/Emotion/app/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'mon-repere-emotions-v0.9.14';
+const CACHE = 'mon-repere-emotions-v0.9.16';
 const CORE = [
   './', './index.html', './app.js', './style.css', './manifest.json',
   './scientific_knowledge_base/data/emotions.json',
