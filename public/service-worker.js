aa03ffaf531f32a3cc29d6c520cb6ccfece2d0e3
PGR Studio <rmwcz7zzn6@privaterelay.appleid.com>
2026-09-07 12:03:27 +0200

    Compact beta home on mobile

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'mon-repere-emotions-v0.9.43-beta';
+const CACHE = 'mon-repere-emotions-v0.9.44-beta';
 const CORE = [
   './', './index.html', './app.js', './style.css', './style.css?v=design-system-2', './manifest.json',
   './scientific_knowledge_base/data/emotions.json',
